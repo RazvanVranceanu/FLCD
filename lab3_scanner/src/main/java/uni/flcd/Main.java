2164027691144c51986d8e71ab793246a1ac6859
RazvanVranceanu <razvavranceanu@yahoo.com>
2022-11-05 20:45:53 +0200

    [LAb3][LAB1A] Updated to code examples

--- lab3_scanner/src/main/java/uni/flcd/Main.java
+++ lab3_scanner/src/main/java/uni/flcd/Main.java
@@ -7,9 +7,6 @@ import uni.flcd.structures.SymbolTable;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: 02.11.2022 mai verifica daca ST si PIF sunt ok pt toate exemplele
-        // TODO: 02.11.2022 versiunea finala la programe
-        // TODO: 02.11.2022 documentatie
         SymbolTable symbolTable = new SymbolTable();
         ProgramInternalForm programInternalForm = new ProgramInternalForm();
         ReservedWords reservedWords = new ReservedWords("src/main/resources/token.in");
